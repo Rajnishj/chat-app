0fcc28191e88d195000cad02cc3331962753b604
Rajnishj <rajnishj05@gmail.com>
2024-12-18 21:36:07 +0530

    fixed login issue

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
 import MessageContainer from "../../components/messages/MessageContainer";
 import Sidebar from "../../components/sidebar/Sidebar";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate()
+  useEffect(() => {
+    const isAuth = localStorage.getItem("isAuthenticated")
+    if(!isAuth){
+      navigate("/login")
+    }
+  },[])
   return (
     <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
       <Sidebar />
